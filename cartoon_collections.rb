bc9688407377fb3549a194156e0ff19cfef152b7
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-15 00:02:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,8 @@ def long_planeteer_calls(arrays)
     word.length > 4 
    end 
 end
-
+ long_planeteer_calls(short_words)
+long_planeteer_calls(assorted_words
 
 def find_the_cheese# code an argument here
   # the array below is here to help
