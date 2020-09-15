72d6a6885610f8c9c1134387bb1278564976a920
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-15 00:02:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  
+def long_planeteer_calls(arrays)
+  array.any? do |word| 
+    word.length > 4 
+   end 
 end
 
 def find_the_cheese# code an argument here
